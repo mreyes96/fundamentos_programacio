1d52c76a9d70197feea1db9f50975e9cdc03bf47
mreyes <mkreyes1506@gmail.com>
2023-12-18 23:14:54 -0500

    se sube los ejercicios de programacion

---
+++ ejercicio2.js
@@ -0,0 +1,21 @@
+// Primer ejercicio
+var nombreEmpleado ="Erick";
+var sueldo = 450;
+// imprimir variable en lineas distintas
+console.log("Nombre del empleado:" +nombreEmpleado);
+console.log("Sueldo:"+sueldo);
+
+//Segundo ejercicio
+var precioArticulo =10;
+var cantidadCliente=5;
+
+var montoTotal = precioArticulo*cantidadCliente;
+
+console.log ("El comprador debe pagar: $"  +montoTotal);
+
+// Tercer ejercicio
+typeof "John"
+typeof 3.14
+typeof false
+typeof {name: 'JOhn', age: 34}
+
